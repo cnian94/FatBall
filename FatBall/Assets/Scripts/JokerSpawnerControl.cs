b0b56e11bc2fe828735882989d49bc1361e4dca1
cnian94 <cnian94@gmail.com>
2018-07-11 21:56:59 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/cnian94/FatBall""
    This reverts commit ab35eaaf544bc74ff7195f2a6725a92326113875.

--- FatBall/Assets/Scripts/JokerSpawnerControl.cs
+++ FatBall/Assets/Scripts/JokerSpawnerControl.cs
@@ -11,26 +11,11 @@ public class JokerSpawnerControl : MonoBehaviour {
 
     public GameObject joker;
     public int num_of_jokers = 0;
-    public int joker_limit = 50;
 
     // Use this for initialization
     void Start()
     {
-        InvokeRepeating("SpawnAMonster", 0f, 1f);
-    }
 
 
-    public void SpawnAJoker()
-    {
-        if (num_of_jokers < joker_limit)
-        {
-            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
-            randomJoker = Random.Range(0, jokers.Length);
-            joker = Instantiate(jokers[randomJoker], spawnPoints[randomSpawnPoint].position,
-                Quaternion.identity);
-            joker.name = "Joker";
-            num_of_jokers++;
-            //Debug.Log("ACTIVE MONSTERS: " + num_of_monsters);
-        }
     }
 }
