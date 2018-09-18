1ef3be26c70809a5c010f53d7fbf720dff97530c
cnian94 <cnian94@gmail.com>
2018-09-18 12:44:15 +0300

    background optimization

---
+++ FatBall/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector3 pos = transform.localPosition;
+        pos.x = Screen.width / 2;
+        pos.y = Screen.height / 2;
+
+        transform.localPosition = pos;
+
+        Vector3 scale = transform.localScale;
+        scale.x = Screen.width;
+        scale.y = Screen.height;
+
+        transform.localScale = scale;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
